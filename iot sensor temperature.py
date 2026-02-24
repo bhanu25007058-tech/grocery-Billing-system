f01b3890c6211d15e1240ad278ea6b840d9cd889
bhanu25007058-tech <bhanu.25007058@kiet.edu>
2026-02-24 13:21:58 +0530

    Create iot sensor temperature.py

---
+++ iot sensor temperature.py
@@ -0,0 +1,18 @@
+# bhanu pratap sharma,202501100700058,sec-b
+import random
+import time
+min_temp = float(input("Enter minimum temperature limit: "))
+max_temp = float(input("Enter maximum temperature limit: "))
+reading = 1
+while reading <= 5:
+    temp = random.randint(0, 100)
+    print("Current temperature:", temp)
+    if temp > max_temp:
+        print("Alert: Temperature is too high")
+    elif temp < min_temp:
+        print("Alert: Temperature is too low")
+    else:
+        print("Temperature is within acceptable limit")
+    print()
+    time.sleep(2)
+    reading = reading + 1
